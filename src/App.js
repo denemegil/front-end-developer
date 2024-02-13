ddcfa08aaef2b5c75162f90716c8ec1a5cc8b309
Oguz ARDUC <oguz.arduc@red.novina.tech>
2024-02-13 04:30:38 +0300

    Project commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {
+  Route, 
+  Routes
+} from 'react-router-dom';
+import pages from './utils/pages';
+import Layout from './components/layout/Layout';
+import Home from './components/pages/Home';
+import Bookings from './components/pages/Bookings';
+import ConfirmedBooking from './components/pages/Bookings/ConfirmedBooking';
+import Error404 from './components/pages/Error404';
+
+const App = () => {
+  return (
+    <>
+      <Layout>
+        <Routes>
+          <Route path={pages.get('home').path} element={<Home />} />
+          <Route path={pages.get('bookings').path} element={<Bookings />} />
+          <Route 
+            path={pages.get('confirmedBooking').path} 
+            element={<ConfirmedBooking />} 
+          />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </Layout>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/pages/Error404/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+const Error404 = () => {
+  return (
+    <div className="container page-not-found">
+      <h2>Page not found</h2>
+    </div>
+  );
+};
+
+export default Error404;
